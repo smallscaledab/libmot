971b53b8f961f911b8cce11612a2e363b866ac1f
ben.poor <ben.poor@thisisglobal.com>
2016-03-28 14:20:53 +0100

    added missing headers

---
+++ include/mot/charsets.h
@@ -0,0 +1,27 @@
+#ifndef CHARSETS_H_
+#define CHARSETS_H_
+
+#include "../include/mot.h"
+
+namespace mot
+{
+	typedef int Charset;
+
+    namespace Charsets
+    {
+        namespace EBU
+        {
+            const Charset Latin = 0;
+            const Charset Latin_Common_Core = 1;
+            const Charset Latin_Core = 2;
+        }
+        namespace ISO
+        {
+            const Charset Latin2 = 3;
+            const Charset Latin1 = 4;
+            const Charset IEC_10646 = 15;
+        }
+    }
+}
+
+#endif /* CHARSETS_H_ */
