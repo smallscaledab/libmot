3359db84d1a8036e92e25141f89f470ab0cd38cd
Ben Poor <ben.poor@thisisglobal.com>
2016-07-16 20:35:52 +0100

    added example as test

--- tests/test_example.cpp
+++ tests/test_example.cpp
@@ -1,23 +1,3 @@
-libmot
-======
-
-Encoder/Decoder for MOT objects as per ETSI EN 301 234 .
-
-# Current Status
-
-Encoding of MOT objects and parameters into Segmented data. 
-
-Used in conjunction with the `libmsc` library (https://github.com/magicbadger/libmsc) to create MSC datagroups and packets from MOT objects.
-
-Currently under development, so subject to changes and refactoring.
-
-# TODO
-
-# Examples
-
-Encoding a simple text MOT object into Datagroups and write out the resultant data as hex pairs for each segment to the console.
-
-```cpp
 #include <vector>
 #include <iostream>
 
@@ -31,7 +11,7 @@ int main() {
     string data("=====");
     vector<unsigned char> bytes;
     copy(data.begin(), data.end(), back_inserter(bytes));
-    int transportId = 8541;
+    int transportId = 8541; 
     MotObject o(transportId, "TestObject", bytes, ContentTypes::Text::ASCII);
     o.AddParameter(new MimeType("application/txt"));
     SegmentEncoder encoder;
@@ -42,8 +22,3 @@ int main() {
     }
     return 0;
 }
-```
-
-# building
-
-automake --add-missing
